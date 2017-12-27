8d37f5cda5c8f28a2768deed2a995082ca6b5513
JohnInterred <j.r.moody159@gmail.com>
2017-12-27 15:10:28 +0000

    Added association and validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,8 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect 
     @article = Article.new(article_params)
+    # hack to maintain integrity before we've built the rest of the User functionality
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article saved successfully"
       redirect_to article_path(@article)
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227125856) do
+ActiveRecord::Schema.define(version: 20171227144047) do
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
